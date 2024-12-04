122982a67a278fc0725f1524f8a337c7fabd57d0
jborruel308@me.com <jborruel308@me.com>
2024-12-03 21:56:03 -0800

    setup for forms on sign in and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from 'react'
 import {Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import SignupForm from './components/SignupForm/SignupForm';
+import SigninForm from './components/SigninForm/SigninForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard'
+import * as authService from '../src/services/authService'
 import './App.css'
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(authService.getUser());
+  
+  const handleSignout = () => {
+    authService.signout()
+    setUser(null)
+  }
   
   return (
     <>
-    <NavBar user={user} />
+    <NavBar user={user}  handleSignout={handleSignout}/>
     <Routes>
-      {user ? (
+      {user ? 
         <Route path="/" element={<Dashboard user={user}/>}/>
-      ): (
+      : 
         <Route path="/" element={<Landing />} />
-      )}
+      }
+      <Route path='/signup' element={<SignupForm  setUser={setUser}/>} />
+      <Route path='/signin' element={<SigninForm setUser={setUser}/>}/>
     </Routes>
 
     </>
   )
-}
+};
 export default App
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ user }) => {
+const NavBar = ({ user, handleSignout }) => {
     return (
         <>
         { user ? (
             <nav>
                 <ul>
                     <li> <Link to="/">Home</Link></li>
-                    <li><Link to="">Sing Out</Link></li>
+                    <li><Link to="" onClick={handleSignout}>Sing Out</Link></li>
                 </ul>
             </nav>
         ):(
